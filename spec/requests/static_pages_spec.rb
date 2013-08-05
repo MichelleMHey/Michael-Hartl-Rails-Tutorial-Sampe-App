f378de006cbc5f81e0b4a3dba52a91227ca874b5
MichelleMHey <michellemhey@gmail.com>
2013-08-05 16:23:17 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,34 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  subject { page }
-
   describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    before { visit root_path }    
+    it { page.should have_selector('h1', :text => 'Sample App') }
+    it { page.should have_selector('title', :text => full_title('')) }
+    it { page.should_not have_selector('title', :text => "| Home") }
   end
 
   describe "Help page" do
-    before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    before { visit help_path }    
+    it { page.should have_selector('h1', :text => 'Help') }
+    it { page.should have_selector('title', :text => full_title('')) }
+    it { page.should_not have_selector('title', :text => "| Help") }
   end
 
   describe "About page" do
-    before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    before { visit about_path }    
+    it { page.should have_selector('h1', :text => 'About Us') }
+    it { page.should have_selector('title', :text => full_title('')) }
+    it { page.should_not have_selector('title', :text => "| About Us") }
   end
 
   describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    before { visit contact_path }    
+    it { page.should have_selector('h1', :text => 'Contact') }
+    it { page.should have_selector('title', :text => full_title('')) }
+    it { page.should_not have_selector('title', :text => "| Contact") }
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe "User pages" do
+
   subject { page }
-    describe "signup page" do
+
+  describe "signup page" do
       before { visit signup_path }
-      it { should have_content('Sign up') }
-      it { should have_title(full_title('Sign up')) }
-    end
+      it { should have_selector('h1', text: 'Sign up') }
+      it { should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Sign up') }
+  end
 end
