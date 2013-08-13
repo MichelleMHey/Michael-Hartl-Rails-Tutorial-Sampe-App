0247aa418ee7d5b5e185945a4a5269930f85c0b2
MichelleMHey <michellemhey@gmail.com>
2013-08-13 13:42:30 -0500

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  
-  #let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  
+describe "StaticPages" do  
 
   subject { page }
 
@@ -37,20 +34,5 @@ describe "StaticPages" do
     it { should have_content('Contact') }
     it { should have_title('') }
     it { should_not have_title('| Contact') }
-  end
-
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    expect(page).to have_title(full_title('About Us'))
-    click_link "Help"
-    expect(page).to # fill in
-    click_link "Contact"
-    expect(page).to # fill in
-    click_link "Home"
-    click_link "Sign up now!"
-    expect(page).to # fill in
-    click_link "sample app"
-    expect(page).to # fill in
   end
 end
