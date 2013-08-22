e9b5a00b92e32a8314eac44a74456c1f9b502fa5
MichelleMHey <michellemhey@gmail.com>
2013-08-21 21:01:08 -0500

    made session controller, view, and rspec tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root              to: 'static_pages#home'
-  match '/home',    to: 'static_pages#home',    via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'ated -> highest priority.
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
